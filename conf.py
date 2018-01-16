4f7b97f1174ede58cdc0ca85bec2ec0631e05f21
AbnerZheng <abnerzheng@gmail.com>
2018-01-16 17:15:11 +0800

    add chinese

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ release = u'1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -175,5 +175,6 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+locale_dirs = ['locale/']
 
 
